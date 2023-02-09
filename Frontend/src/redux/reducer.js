27f8f62a4eb6cee61dcc1552ae874778cf2ca396
MatiasPiris17 <matiaslinkedin17@gmail.com>
2023-02-09 13:08:19 -0300

    React-Redux integrado

--- Frontend/src/redux/reducer.js
+++ Frontend/src/redux/reducer.js
@@ -1,26 +1,30 @@
 import { ADD_FAVORITE, DELETE_FAVORITE } from "./actions-types";
 
 const initialState = {
-    myFavorites:[]
-}
+  myFavorites: [],
+  allCharacters:[]
+};
 
-const rootReducer = (state=initialState, action) => {//{type,playload} 
-    switch(action.type){
-        case ADD_FAVORITE:
-            return{
-                ...state,
-                myFavorites:[...state.myFavorites, action.payload]
-            }
-        case DELETE_FAVORITE:
-            return{
-                ...state,
-                myFavorites:state.myFavorites.filter((char)=>char.id !== action.payload)
-            }
+const rootReducer = (state = initialState, action) => {
+  //{type,playload}
+  switch (action.type) {
+    case ADD_FAVORITE:
+      return {
+        ...state,
+        myFavorites: [...state.myFavorites, action.payload],
+        allCharacters: [...state.myFavorites], 
+      };
+    case DELETE_FAVORITE:
+      return {
+        ...state,
+        myFavorites: state.myFavorites.filter(
+          (char) => char.id !== action.payload
+        ),
+      };
 
-        default:
-            return{...state}
-    }
-} 
-
+    default:
+      return { ...state };
+  }
+};
 
 export default rootReducer;
